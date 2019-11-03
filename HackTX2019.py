402764014222a5db5cdf7f0be8074895681bcbdb
Kevin Cherucheril <kevinphilipc@hotmail.com>
2019-11-02 22:22:15 -0500

    Added code to read datasets

--- HackTX2019.py
+++ HackTX2019.py
@@ -1,141 +1,61 @@
 #Get data from Spotify
-
-import os
-import spotipy
-import spotipy.util as util
 import numpy as np
-from json.decoder import JSONDecodeError
 
+test_x = "x0_test.csv"
+test_y = "y0_test.csv"
+train_x = "x0_train.csv"
+train_y = "y0_train.csv"
 
-username = "kevinphilipc"
-user_playlist = "Train"
-scope = 'playlist-read-private'
-try:
-    token = util.prompt_for_user_token(username,scope,client_id='d7f8b5638bde46cd9f6089a637586e61',client_secret='e04315c74de9416599a087895e24b01b',redirect_uri='http://localhost/')
-except (AttributeError, JSONDecodeError):
-    #os.remove(f".cache-{username}")
-    token = util.prompt_for_user_token(username,scope,client_id='d7f8b5638bde46cd9f6089a637586e61',client_secret='e04315c74de9416599a087895e24b01b',redirect_uri='http://localhost/')
+f = open(test_x, "r")
+lines = f.readlines()
+lines = [i.split(",") for i in lines]
+for i in range(len(lines)):
+    for j in range(len(lines[i])):
+        lines[i][j].replace("e", "E")
+        lines[i][j] = float(lines[i][j])
+f.close()
 
-song_ids = []
-features = []
+X_test = np.array(lines)
 
-if token:
-    sp = spotipy.Spotify(auth=token)
-    playlists = sp.user_playlists(username)
-    for playlist in playlists['items']:
-        if playlist['name'] == user_playlist:
-            results = sp.user_playlist(username, playlist['id'], fields="tracks,next")
-            tracks = results['tracks']
-            for item in tracks['items']:
-                track = item['track']
-                song_ids.append(track['id'])
-                a = track['id']
-                feat = sp.audio_features(a)[0]
-                features.append(feat)
-else:
-    print("Can't get token for", username)
+f = open(test_y, "r")
+lines = f.readlines()
+lines = [i.split(",") for i in lines]
+for i in range(len(lines)):
+    for j in range(len(lines[i])):
+        lines[i][j].replace("e", "E")
+        lines[i][j] = float(lines[i][j])
+f.close()
 
+Y_test = np.array(lines)
 
-#print(song_ids)
-#print(features)
+f = open(train_x, "r")
+lines = f.readlines()
+lines = [i.split(",") for i in lines]
+for i in range(len(lines)):
+    for j in range(len(lines[i])):
+        lines[i][j].replace("e", "E")
+        lines[i][j] = float(lines[i][j])
+f.close()
 
-n = len(song_ids)
-X = np.zeros((8, n))
+X = np.array(lines)
 
-for i in range(n):
-    dance = features[i]['danceability']
-    energy = features[i]['energy']
-    mode = float(features[i]['mode'])
-    speech = features[i]['speechiness']
-    acoustic = features[i]['acousticness']
-    instrumental = features[i]['instrumentalness']
-    liveness = features[i]['liveness']
-    valence = features[i]['valence']
+f = open(train_y, "r")
+lines = f.readlines()
+lines = [i.split(",") for i in lines]
+for i in range(len(lines)):
+    for j in range(len(lines[i])):
+        lines[i][j].replace("e", "E")
+        lines[i][j] = float(lines[i][j])
+f.close()
 
-    X[0][i] = dance
-    X[1][i] = energy
-    X[2][i] = mode
-    X[3][i] = speech
-    X[4][i] = acoustic
-    X[5][i] = instrumental
-    X[6][i] = liveness
-    X[7][i] = valence
-
-
-
-Y = np.zeros((n, 1))
-for i in range(n//2):
-    Y[i][0] = 1
-
-
-
-username = "kevinphilipc"
-user_playlist = "Test"
-scope = 'playlist-read-private'
-try:
-    token = util.prompt_for_user_token(username,scope,client_id='d7f8b5638bde46cd9f6089a637586e61',client_secret='e04315c74de9416599a087895e24b01b',redirect_uri='http://localhost/')
-except (AttributeError, JSONDecodeError):
-    #os.remove(f".cache-{username}")
-    token = util.prompt_for_user_token(username,scope,client_id='d7f8b5638bde46cd9f6089a637586e61',client_secret='e04315c74de9416599a087895e24b01b',redirect_uri='http://localhost/')
-
-song_ids = []
-features = []
-
-if token:
-    sp = spotipy.Spotify(auth=token)
-    playlists = sp.user_playlists(username)
-    for playlist in playlists['items']:
-        if playlist['name'] == user_playlist:
-            results = sp.user_playlist(username, playlist['id'], fields="tracks,next")
-            tracks = results['tracks']
-            for item in tracks['items']:
-                track = item['track']
-                song_ids.append(track['id'])
-                a = track['id']
-                feat = sp.audio_features(a)[0]
-                features.append(feat)
-else:
-    print("Can't get token for", username)
-
-m = len(song_ids)
-X_test = np.zeros((8, m))
-
-for i in range(m):
-    dance = features[i]['danceability']
-    energy = features[i]['energy']
-    mode = float(features[i]['mode'])
-    speech = features[i]['speechiness']
-    acoustic = features[i]['acousticness']
-    instrumental = features[i]['instrumentalness']
-    liveness = features[i]['liveness']
-    valence = features[i]['valence']
-
-    X_test[0][i] = dance
-    X_test[1][i] = energy
-    X_test[2][i] = mode
-    X_test[3][i] = speech
-    X_test[4][i] = acoustic
-    X_test[5][i] = instrumental
-    X_test[6][i] = liveness
-    X_test[7][i] = valence
-
-
-
-Y_test = np.zeros((m, 1))
-for i in range(m//2):
-    Y_test[i][0] = 1
-
-Y= Y.T
-Y_test = Y_test.T
+Y = np.array(lines)
 
 #-------------------------------------------------------------------------------------------------------------------------------
 # Package imports
-import numpy as np
-
 
 np.random.seed(1) # set a seed so that the results are consistent
 
-    
+
 def sigmoid(x):
     s = 1/(1+np.exp(-x))
     return s
@@ -156,24 +76,24 @@ def layer_sizes(X, Y):
 
 
 
-def initialize_parameters(n_x, n_h, n_y):    
+def initialize_parameters(n_x, n_h, n_y):
 
     W1 = np.random.randn(n_h, n_x)*.01
     b1 = np.zeros((n_h, 1))
     W2 = np.random.randn(1, n_h)*.01
     b2 = np.zeros((1,1))
 
-    
+
     assert (W1.shape == (n_h, n_x))
     assert (b1.shape == (n_h, 1))
     assert (W2.shape == (n_y, n_h))
     assert (b2.shape == (1,1))
-    
+
     parameters = {"W1": W1,
                   "b1": b1,
                   "W2": W2,
                   "b2": b2}
-    
+
     return parameters
 
 
@@ -190,35 +110,35 @@ def forward_propagation(X, parameters):
     Z2 = np.dot(W2, A1) + b2
     A2 = sigmoid(Z2)
 
-    
+
     assert(A2.shape == (1, X.shape[1]))
-    
+
     cache = {"Z1": Z1,
              "A1": A1,
              "Z2": Z2,
              "A2": A2}
-    
+
     return A2, cache
 
 
 
 def compute_cost(A2, Y, parameters):
-    
+
     m = Y.shape[1]
 
     cost = -1/m*np.sum(Y*np.log(A2) + (1-Y)*np.log(1-A2), axis = 1)
 
-    
+
     cost = float(np.squeeze(cost))
     assert(isinstance(cost, float))
-    
+
     return cost
 
 
 
 def backward_propagation(parameters, cache, X, Y):
     m = X.shape[1]
-    
+
     W1 = parameters["W1"]
     W2 = parameters["W2"]
 
@@ -232,12 +152,12 @@ def backward_propagation(parameters, cache, X, Y):
     dZ1 = np.dot(W2.T, dZ2)*(1 - np.power(A1, 2))
     dW1 = 1/m*np.dot(dZ1, X.T)
     db1 = 1/m*np.sum(dZ1, axis = 1, keepdims = True)
-    
+
     grads = {"dW1": dW1,
              "db1": db1,
              "dW2": dW2,
              "db2": db2}
-    
+
     return grads
 
 
@@ -250,52 +170,52 @@ def update_parameters(parameters, grads, learning_rate = 1.2):
     W2 = parameters["W2"]
     b2 = parameters["b2"]
 
-    
+
 
     dW1 = grads["dW1"]
     db1 = grads["db1"]
     dW2 = grads["dW2"]
     db2 = grads["db2"]
-    
-    
+
+
 
     W1 = W1 - learning_rate * dW1
     b1 = b1 - learning_rate * db1
     W2 = W2 - learning_rate * dW2
     b2 = b2 - learning_rate * db2
-    
+
     parameters = {"W1": W1,
                   "b1": b1,
                   "W2": W2,
                   "b2": b2}
-    
+
     return parameters
 
 
 def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False, learning_rate = 1.2):
     n_x = layer_sizes(X, Y)[0]
     n_y = layer_sizes(X, Y)[2]
-    
+
 
     parameters = initialize_parameters(n_x, n_h, n_y)
 
-    
+
 
 
     for i in range(0, num_iterations):
-         
+
 
         A2, cache = forward_propagation(X, parameters)
-        
+
 
         cost = compute_cost(A2, Y, parameters)
- 
+
 
         grads = backward_propagation(parameters, cache, X, Y)
- 
+
 
         parameters = update_parameters(parameters, grads, learning_rate)
-        
+
         if print_cost and i % 1000 == 0:
             print ("Cost after iteration %i: %f" %(i, cost))
 
@@ -303,11 +223,11 @@ def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False, learning_rate 
 
 
 def predict(parameters, X):
-    
+
     A2, cache = forward_propagation(X, parameters)
     predictions = A2 > 0.5
 
-    
+
     return predictions
 
 
